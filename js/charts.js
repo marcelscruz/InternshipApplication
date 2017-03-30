ecaf2ab7cc7681db8ae8ccb6694081b7a93583ee
Marcel Cruz <marcelcruz@live.com>
2017-03-31 00:24:00 +0100

    code tidy and working

---
+++ js/charts.js
@@ -0,0 +1,17 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["HTML", "CSS", "JavaScript", "Illustrator", "Photoshop"],
+				datasets: [{
+					backgroundColor: [
+						"#e74c3c",
+						"#3498db",
+						"#f1c40f",
+						"#9b59b6",
+						"#2ecc71"
+					],
+					data: [8, 7, 5, 5, 4]
+				}]
+			}
+		});
--- js/script.js
+++ js/script.js
@@ -1,43 +1,19 @@
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				scrollingSpeed: 1000
-			});
-	
-		$.fn.visible = function(partial) {
-    
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height(),
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
-    
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-  };
-    
-})(jQuery);
 
-var win = $(window);
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
-var allMods = $(".module");
+    if (scroll < 300) {
+		$("#section2").fadeOut();
+		$("#section1").fadeIn();			
 
-allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass("already-visible"); 
-  } 
-});
+		
+    } else if (scroll >= 300 && scroll < 1000) {
+		
+		$("#section1").fadeOut();
+		$("#section2").fadeIn();
 
-win.scroll(function(event) {
-  
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("come-in"); 
-    } 
-  });
-  
-});
+	} else {
+
+	}
+    });
