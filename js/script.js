c9ea6cc9fb86717d67243384e503f17fde824a43
Marcel Cruz <marcelcruz@live.com>
2017-04-03 23:25:44 +0100

    js file updated with functions to add and remove classes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
-	window.scrollBy(-1000, -1000);
-
+	// disables scrolling
 	$('body').on('scroll touchmove mousewheel', function (e) {
 		e.preventDefault();
 		e.stopPropagation();
@@ -15,61 +14,102 @@ function scrollUp() {
 	window.scrollBy(0, -1000);
 };
 
+
 function scrollDown() {
 	window.scrollBy(0, 1000);
 };
 
+
+function addClassSection (idInput1, idInput2, classInput) {
+	
+	var idInput1 = "#" + idInput1;
+	var idInput2 = "#" + idInput2;
+	
+	if (!$(idInput1).hasClass(classInput)) {
+		$(idInput1).fadeOut().addClass(classInput);
+	} else if (!$(idInput2).hasClass(classInput)) {
+		$(idInput2).fadeOut().addClass(classInput);
+	}
+}
+
+
+function rmvClassSection (idInput, classInput) {
+	
+	var idInput = "#" + idInput;
+	
+	if ($(idInput).hasClass(classInput)) {
+		$(idInput).removeClass(classInput).fadeIn();
+	}
+}
+
+
+function addClassBtn (idInput, classInput) {
+	
+	var idInput = "#" + idInput;
+	
+	if (!$(idInput).hasClass(classInput)) {
+		$(idInput).addClass(classInput);
+	} 	
+}
+
+
+function rmvClassBtn (idInput, classInput) {
+	
+	var idInput = "#" + idInput;
+	
+	if ($(idInput).hasClass(classInput)) {
+		$(idInput).removeClass(classInput);
+	}
+}
+
+
 $(window).scroll(function () {
 
 	var scroll = $(window).scrollTop();
-
+	
 	if (scroll < 1000) {
 
-		$("#section2").fadeOut();
-//		$("#section3").fadeOut();
-//		$("#section4").fadeOut();
-//		$("#section5").fadeOut();
-		$("#buttonUp").addClass("buttonPressed");
-		$("#section1").fadeIn();
-
+		// SECTION 1
+		
+		addClassSection("section2", null, "hidden");
+		rmvClassSection("section1", "hidden");
+		
+		addClassBtn("buttonUp", "buttonPressed");
+		
 	} else if (scroll >= 1000 && scroll < 2000) {
 
-		$("#section2").removeClass("hidden");
-		$("#section3").removeClass("hidden");
-		$("#section4").removeClass("hidden");
-		$("#section5").removeClass("hidden");
-		$("#buttonUp").removeClass("buttonPressed");
-		$("#section1").fadeOut();
-		$("#section3").fadeOut();
-		$("#section4").fadeOut();
-		$("#section5").fadeOut();
-		$("#section2").fadeIn();
-
+		// SECTION 2
+		
+		addClassSection("section1", "section3", "hidden");
+		rmvClassSection("section2", "hidden");
+		
+		rmvClassBtn("buttonUp", "buttonPressed");
+		
 	} else if (scroll >= 2000 && scroll < 3000) {
 
-		$("#section1").fadeOut();
-		$("#section2").fadeOut();
-		$("#section4").fadeOut();
-		$("#section5").fadeOut();
-		$("#section3").fadeIn();
+		// SECTION 3
+		
+		addClassSection("section2", "section4", "hidden");
+		rmvClassSection("section3", "hidden");
 
 	} else if (scroll >= 3000 && scroll < 4000) {
 
-		$("#buttonDown").removeClass("buttonPressed");
-		$("#section1").fadeOut();
-		$("#section2").fadeOut();
-		$("#section3").fadeOut();
-		$("#section5").fadeOut();
-		$("#section4").fadeIn();
-
+		// SECTION 4
+		
+		addClassSection("section3", "section5", "hidden");
+		rmvClassSection("section4", "hidden");
+		
+		rmvClassBtn("buttonDown", "buttonPressed");
+	
+		
 	} else if (scroll >= 4000 && scroll < 5000) {
 
-		$("#buttonDown").addClass("buttonPressed");
-		$("#section1").fadeOut();
-		$("#section2").fadeOut();
-		$("#section3").fadeOut();
-		$("#section4").fadeOut();
-		$("#section5").fadeIn();
+		// SECTION 5
+		
+		addClassSection("section4", null, "hidden");
+		rmvClassSection("section5", "hidden");
+		
+		addClassBtn("buttonDown", "buttonPressed");
 
 	} else {}
 
